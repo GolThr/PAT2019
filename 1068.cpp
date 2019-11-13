23bda6b8bedd22e53574b0e021d33cba05407c61
GolThr <1344006993@qq.com>
2019-11-13 18:13:02 +0800

    Update 1068.cpp

--- 1068.cpp
+++ 1068.cpp
@@ -1,50 +1,66 @@
+/*****************************
+*注意：
+ 1. 不仅要满足和周围8个像素点颜色差充分大，还要满足这个点的颜色在所有里面是独一无二的 
+*参考：
+ 1. [CSDN] PAT 1068. 万绿丛中一点红(20)
+	https://blog.csdn.net/qq_22530055/article/details/60469575
+*****************************/ 
+
 #include <iostream>
 #include <cmath>
 using namespace std;
- 
-int main(){
+
+int value[20000000] = {0};
+
+int main() {
 	int m, n, tol, i, j, k, l, cnt = 0;
 	cin >> m >> n >> tol;
-	int color[m][n];
+	int color[n][m];
 	bool flag = true;
 	int rx, ry;
-	
-	for(i = 0; i < m; i++){
-		for(j = 0; j < n; j++){
+
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < m; j++) {
 			cin >> color[i][j];
+			value[color[i][j]]++;
 		}
 	}
-	for(i = 0; i < m; i++){
-		for(j = 0; j < n; j++){
-			for(k = i - 1; k <= i + 1; k++){
-				if(k >= 0){
-					for(l = j - 1; l <= j + 1; l++){
-						if(l >= 0 && k != i && l != j){
-							flag = true;
-							if(abs(color[i][j] - color[k][l]) <= tol){
-								flag = false;
-								break;
+	for(i = 0; i < n; i++) {
+		for(j = 0; j < m; j++) {
+			if(value[color[i][j]] == 1) {
+				for(k = i - 1, flag = true; k <= i + 1; k++) {
+					if(k >= 0 && k < n && flag) {
+						for(l = j - 1; l <= j + 1; l++) {
+							if(l >= 0 && l < m && (k != i || l != j)) {
+								//flag = true;
+								if(abs(color[i][j] - color[k][l]) <= tol) {
+									flag = false;
+									//cout << k << l << flag << " ";
+									break;
+								}
+								//cout << k << l << flag << " ";
+								//cout << k << l << flag << color[i][j] << "-" << color[k][l] << " ";
 							}
 						}
 					}
 				}
-			}
-			if(flag == true){
-				cnt++;
-				rx = i;
-				ry = j;
-				cout << i << j << endl;
+				//cout << i << j << "end" << endl;
+				if(flag == true) {
+					cnt++;
+					rx = i;
+					ry = j;
+				}
 			}
 		}
 	}
-	cout << cnt << endl;
-	if(cnt == 1){
-		cout << "(" << ry << ", " << rx << "): " << color[rx][ry];
-	}else if(cnt > 1){
+	//cout << cnt << endl;
+	if(cnt == 1) {
+		cout << "(" << ry + 1 << ", " << rx + 1 << "): " << color[rx][ry];
+	} else if(cnt > 1) {
 		cout << "Not Unique";
-	}else{
+	} else {
 		cout << "Not Exist";
 	}
-	
+
 	return 0;
-} 
+}
